0d7d0b416ddd2ba61e2c417931e8d4229a1631a3
Sparshisthebest <sparsh.nayak0812@gmail.com>
2025-08-26 11:38:13 +0100

    Add files via upload

---
+++ activity1_3.py
@@ -0,0 +1,13 @@
+tree1= 58
+tree2= 67
+tree3= 89
+tree4= 34
+tree5= 90
+
+# Finding the total of trees
+sum=tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 trees : ",sum)
+
+# finding the average of the trees
+average = sum/5
+print("the average of the trees is :" ,average)
